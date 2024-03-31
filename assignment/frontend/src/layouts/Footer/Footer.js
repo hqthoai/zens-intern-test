ace5feed02bd633bd2ed298e6c2e582f0459f312
Quoc Thoai <thoaifamily83@gmail.com>
2024-03-31 23:15:37 +0700

    change text color of footer

--- assignment/frontend/src/layouts/Footer/Footer.js
+++ assignment/frontend/src/layouts/Footer/Footer.js
@@ -1,14 +1,14 @@
 function Footer() {
     return (
         <div className="px-[220px] py-10 text-center font-sans border-t-[2px]">
-            <p className="text-neutral-500 ">
+            <p className="text-gray-500 ">
                 This website is created as part of Hisolutions program. The materials contained on this website are provided for general
                 <br />
                 information only and do not constitute any form of advice.HLS assumes no responsibility for the accuracy of any particular statement and
                 <br />
                 accepts no liability for any loss or damage which may arise from reliance on the information contained on this site.
             </p>
-            <p className="text-neutral-600 font-medium mt-6">
+            <p className="text-gray-600 font-medium mt-6">
                 Copyright 2021 HLS
             </p>
         </div>
