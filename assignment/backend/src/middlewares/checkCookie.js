2b5299a3b965bb46714488907d82cffedd097d20
Quoc Thoai <thoaifamily83@gmail.com>
2024-04-01 23:02:14 +0700

    check origin url

--- assignment/backend/src/middlewares/checkCookie.js
+++ assignment/backend/src/middlewares/checkCookie.js
@@ -5,6 +5,7 @@ const checkCookie = (req, res, next) => {
     const user = req.cookies.user;
     if (!user) {
         console.log('chưa có cookie nè');
+        console.log(req.originalUrl);
         const newUser = uuidv4();
         const maxAge = calAgeCookie();
         res.cookie('user', newUser, { maxAge: maxAge });
