ceeb4e4fbd474be06a83fe2a69913581fd2fa4a3
Quoc Thoai <thoaifamily83@gmail.com>
2024-04-02 00:38:06 +0700

    fix check cookie

--- assignment/backend/src/middlewares/checkCookie.js
+++ assignment/backend/src/middlewares/checkCookie.js
@@ -2,14 +2,11 @@ const { v4: uuidv4 } = require('uuid');
 const calAgeCookie = require("../utils/calAgeCookie");
 
 const checkCookie = (req, res, next) => {
-    const user = req.cookies.user;
-    console.log('cookies', req.cookies);
     if (!user) {
         console.log('chưa có cookie nè');
-        console.log(req.originalUrl);
         const newUser = uuidv4();
         const maxAge = calAgeCookie();
-        res.cookie('user', newUser, { maxAge: maxAge, sameSite: 'none', }).send();
+        res.cookie('user', newUser, { maxAge: maxAge, sameSite: 'none', });
         return res.redirect(req.originalUrl);
     }
     next();
