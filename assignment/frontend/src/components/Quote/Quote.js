fd156bf3c779e14493d9a0a2eee4025d42c53e61
Quoc Thoai <thoaifamily83@gmail.com>
2024-04-01 12:51:00 +0700

    add shadow inner for quote

--- assignment/frontend/src/components/Quote/Quote.js
+++ assignment/frontend/src/components/Quote/Quote.js
@@ -1,6 +1,6 @@
 function Quote() {
     return (
-        <div className="md:h-[258px] h-[150px] bg-[#2fb363] w-full flex flex-col justify-center items-center text-white">
+        <div className="md:h-[258px] h-[150px] bg-[#2fb363] shadow-[inset_0_4px_0px_0_rgba(0,0,0,0.06)] w-full flex flex-col justify-center items-center text-white">
             <p className="md:text-3xl text-[16px] font-semibold">A joke a day keeps the doctor away</p>
             <p className="md:text-xs text-[10px] font-medium mt-6">If you joke wrong way, your teeth have to pay. (Serious)</p>
         </div>
