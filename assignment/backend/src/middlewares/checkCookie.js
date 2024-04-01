709e61a3d0ac2e33641bf6264e823f0b9210b4e6
Quoc Thoai <thoaifamily83@gmail.com>
2024-04-01 22:53:04 +0700

    log message to check cookie

--- assignment/backend/src/middlewares/checkCookie.js
+++ assignment/backend/src/middlewares/checkCookie.js
@@ -4,6 +4,7 @@ const calAgeCookie = require("../utils/calAgeCookie");
 const checkCookie = (req, res, next) => {
     const user = req.cookies.user;
     if (!user) {
+        console.log('chưa có cookie nè');
         const newUser = uuidv4();
         const maxAge = calAgeCookie();
         res.cookie('user', newUser, { maxAge: maxAge });
