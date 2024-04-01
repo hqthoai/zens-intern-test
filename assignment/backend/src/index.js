1224533631a483e6282599d26c5a705bf17a0683
Quoc Thoai <thoaifamily83@gmail.com>
2024-04-01 20:29:32 +0700

    add cors for deployment url

--- assignment/backend/src/index.js
+++ assignment/backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: ['http://localhost:3000',],
+        origin: ['http://localhost:3000', 'https://zens-intern-test-ui.vercel.app/'],
         methods: 'GET,POST,PUT,PATCH,DELETE',
         allowedHeaders: 'Content-Type,Authorization',
         credentials: true
