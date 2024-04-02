512b5f21ffffe723fc4d7bc8ae504917938cc5fa
Quoc Thoai <thoaifamily83@gmail.com>
2024-04-02 11:07:10 +0700

    add toast message for voting

--- assignment/frontend/src/components/JokeStory/JokeStory.js
+++ assignment/frontend/src/components/JokeStory/JokeStory.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import VoteButton from "./VoteButton/VoteButton";
 import JokeService from "../../services/JokeServices";
 import VoteService from "../../services/VoteService";
@@ -23,35 +26,39 @@ function JokeStory() {
         try {
             await VoteService.voteJoke(joke?._id, type);
             setVote(!vote);
+            toast.success("Thanks for voting!");
         } catch (error) {
             console.error(error);
         }
     };
 
     return (
-        <div className="lg:px-[220px] md:px-[80px] px-7 mt-[68px] mb-[104px] min-h-[200px]">
-            {joke?.message ?
-                <div className="md:min-h-[200px] min-h-[300px] text-gray-700 font-semibold text-xl text-center">
-                    <p>{joke.message}</p>
-                </div>
-                : <React.Fragment>
-                    <div className="md:min-h-[200px] min-h-[300px] text-start relative" >
-                        <p>
-                            {joke?.content}
-                        </p>
-                        <div className="md:block hidden after:block after:content-[''] after:border-b after:w-2/3 after:absolute after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2"></div>
-                    </div >
-
-                    <div className="flex mt-[50px] items-center justify-center">
-                        <div className="mr-8">
-                            <VoteButton content="This is Funny!" color="#2c7edb" onClick={() => handleVote('like')} />
-                        </div>
-                        <VoteButton content="This is not funny." color="#2fb363" onClick={() => handleVote('dislike')} />
+        <React.Fragment>
+            <ToastContainer />
+            <div className="lg:px-[220px] md:px-[80px] px-7 mt-[68px] mb-[104px] min-h-[200px]">
+                {joke?.message ?
+                    <div className="md:min-h-[200px] min-h-[300px] text-gray-700 font-semibold text-xl text-center">
+                        <p>{joke.message}</p>
                     </div>
-                </React.Fragment>
+                    : <div>
+                        <div className="md:min-h-[200px] min-h-[300px] text-start relative" >
+                            <p>
+                                {joke?.content}
+                            </p>
+                            <div className="md:block hidden after:block after:content-[''] after:border-b after:w-2/3 after:absolute after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2"></div>
+                        </div >
 
-            }
-        </div >
+                        <div className="flex mt-[50px] items-center justify-center">
+                            <div className="mr-8">
+                                <VoteButton content="This is Funny!" color="#2c7edb" onClick={() => handleVote('like')} />
+                            </div>
+                            <VoteButton content="This is not funny." color="#2fb363" onClick={() => handleVote('dislike')} />
+                        </div>
+                    </div>
+
+                }
+            </div >
+        </React.Fragment>
     );
 }
 
